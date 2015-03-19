825b420ce79f39af6a5c3e3331e0c32aa8bad7c5
Javi Manzano <javi@potatolondon.com>
2015-03-19 17:48:45 +0000

    Initial Commit

---
+++ HNReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HNReader
+//
+//  Created by Javi Manzano on 19/03/2015.
+//  Copyright (c) 2015 Javi Manzano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
