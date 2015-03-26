aec70a5f1206fbf9830a80599a7b6008adc38d6a
Javi Manzano <javi.manzano.oller@gmail.com>
2015-03-26 21:34:13 +0000

    Remove annoying white bar from webview

--- HNReader/StoryDetailViewController.swift
+++ HNReader/StoryDetailViewController.swift
@@ -24,6 +24,7 @@ class StoryDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.automaticallyAdjustsScrollViewInsets = false
         self.configureView()
     }
     
