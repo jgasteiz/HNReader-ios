083233cbc96cff534454daf48513c577fbaec133
Javi Manzano <javi.manzano.oller@gmail.com>
2015-09-24 22:07:06 +0100

    Add new method to stories task to fetch story detail

--- HNReader/HNStoriesTask.swift
+++ HNReader/HNStoriesTask.swift
@@ -3,10 +3,36 @@ import Foundation
 class HNStoriesTask {
     
     let topNewsURL: NSURL
+    let storyURL: NSURL
     var storiesArray: [Story] = []
+    var storyDetail: NSDictionary = NSDictionary()
     
     init() {
         topNewsURL = NSURL(string: "http://node-hnapi-javiman.herokuapp.com/news")!
+        storyURL = NSURL(string: "http://node-hnapi.herokuapp.com/item/10272483")!
+    }
+    
+    func getStory (id: Int, onTaskDone: () -> Void, onTaskError: () -> Void) {
+        // Get the top stories form the API
+        let sharedSession = NSURLSession.sharedSession()
+        let downloadTask: NSURLSessionDownloadTask = sharedSession.downloadTaskWithURL(storyURL, completionHandler: { (location: NSURL?, response: NSURLResponse?, error: NSError?) -> Void in
+            
+            if error == nil {
+                // Get the url content as NSData
+                let dataObject = NSData(contentsOfURL: location!)
+                // Get the story
+                self.storyDetail = (try! NSJSONSerialization.JSONObjectWithData(dataObject!, options: [])) as! NSDictionary
+                
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    onTaskDone()
+                })
+            } else {
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    onTaskError()
+                })
+            }
+        })
+        downloadTask.resume()
     }
     
     func getTopStories(onTaskDone: () -> Void, onTaskError: () -> Void) {
