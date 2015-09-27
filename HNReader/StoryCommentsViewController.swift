063f3cc89be0af7962e0e3ed0eaa6e1d673480a0
Javi Manzano <javi.manzano.oller@gmail.com>
2015-09-27 13:23:37 +0100

    Display first level comments properly

--- HNReader/StoryCommentsViewController.swift
+++ HNReader/StoryCommentsViewController.swift
@@ -8,18 +8,20 @@
 
 import UIKit
 
-class StoryCommentsViewController: UIViewController {
+class StoryCommentsViewController: UITableViewController {
     
     var hnStoriesTask = HNStoriesTask()
+    
+    var commentList: NSArray = NSArray()
     
     var storyTitle: String?
     var storyId: Int?
     
-    @IBOutlet weak var content: UILabel!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.automaticallyAdjustsScrollViewInsets = false
+        
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
         
         self.navigationItem.title = self.storyTitle
         
@@ -31,16 +33,56 @@ class StoryCommentsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.commentList.count;
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // Get the table cell
+        let cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier("CommentCell") as UITableViewCell!
+        
+        // Retrieve the story with the cell index
+        let comment = self.commentList[indexPath.row] as! NSDictionary
+        let commentText = (comment["content"] as! String).html2String
+        
+        // Get the labels to fill
+        let commentLabel: UILabel = cell.viewWithTag(110) as! UILabel
+        commentLabel.text = commentText
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        let cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier("CommentCell") as UITableViewCell!
+        let commentLabel: UILabel = cell.viewWithTag(110) as! UILabel
+        let comment = self.commentList[indexPath.row] as! NSDictionary
+        let commentText = (comment["content"] as! String).html2String
+        
+        return heightForView(commentText, font: commentLabel.font, width: cell.frame.width)
+    }
+    
+    func heightForView(text: String, font: UIFont, width: CGFloat) -> CGFloat {
+        let label:UILabel = UILabel(frame: CGRectMake(0, 0, width, CGFloat.max))
+        label.numberOfLines = 0
+        label.lineBreakMode = NSLineBreakMode.ByWordWrapping
+        label.font = font
+        label.text = text
+        
+        label.sizeToFit()
+        return label.frame.height
+    }
+    
     func fetchComments() {
         hnStoriesTask.getStory(self.storyId!, onTaskDone: onGetPostsSuccess, onTaskError: onGetPostsError)
     }
     
     func onGetPostsSuccess() {
-        self.content.text = self.hnStoriesTask.storyDetail["comments"]!.description
+        self.commentList = self.hnStoriesTask.storyDetail["comments"] as! NSArray
+        tableView.reloadData()
     }
     
     func onGetPostsError() {
-        self.content.text = "Error"
+        // do something!
         
         // Show error message
         let alertController = UIAlertController(title: "Ooops", message:
